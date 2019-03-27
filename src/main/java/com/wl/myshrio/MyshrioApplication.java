fb5ddaa93d063a23a217e1ea30e511de6b9bb835
wanglong <495250430@qq.com>
2019-03-27 09:25:18 +0800

    Initial commit

---
+++ src/main/java/com/wl/myshrio/MyshrioApplication.java
@@ -0,0 +1,13 @@
+package com.wl.myshrio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyshrioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyshrioApplication.class, args);
+    }
+
+}
